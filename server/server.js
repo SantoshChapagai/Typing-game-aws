3b0f6b76b6a2a1ebc09552fba93afa25f0c3db37
SantoshChapagai <santoshchapagai100@gmail.com>
2023-08-21 20:48:23 +0300

    practice for code pipeline

--- server/server.js
+++ server/server.js
@@ -6,4 +6,4 @@ app.use(express.static('dist'))
 // use port 3000 unless there exists a preconfigured port
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log('Listening on port 3001!'))
+app.listen(port, function () { console.log('Listening on port 3001!') });
